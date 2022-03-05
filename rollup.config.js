8e398e62d5b956d89381f26be3f8760ea173e793
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2022-03-05 18:41:29 +0900

    feat: console.log('hello')

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { terser } from 'rollup-plugin-terser';
+
+const config = (src, dist) => ({
+  input: src,
+  output: {
+    file: dist,
+    format: 'cjs',
+  },
+  plugins: [
+    (process.env.NODE_ENV === 'production' && terser()),
+  ],
+});
+
+export default [
+  config('index.ts', 'index.js'),
+];
